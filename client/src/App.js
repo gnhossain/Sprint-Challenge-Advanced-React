f3c2e8d49c8801aa40559c993c53847447eeaba2
G H <gh@Gs-MacBook-Pro.local>
2019-10-04 10:10:45 -0700

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './App.css';
+import NavBar from './components/NavBar';
+import PlayerCard from './components/PlayerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor()
+  {
+    super();
+    this.state = {
+      players:[]
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.setState({players: res.data});
+        console.log(this.state.players);
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  render()
+  {
+    return (
+      <div className='App'>
+        <NavBar />
+        <div>
+          <h2>Players Ranked By Searches</h2>   
+        </div>
+
+        <h1 className='title'>
+          Players
+        </h1>
+          {this.state.players.map( player => (
+            <PlayerCard player={player} key={player.id}/>
+          ))}
+      </div>
+    );
+  }
 }
-
 export default App;
